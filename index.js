f3834d35cc4f2a22280c6eb54896ae5830381af3
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-31 14:24:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
 
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll("ul.ranked-list li");
+  for(var i = 0; i < ranks.length; i++){ }
+}
+
+
+
+function deepestChild(){
+
+}
